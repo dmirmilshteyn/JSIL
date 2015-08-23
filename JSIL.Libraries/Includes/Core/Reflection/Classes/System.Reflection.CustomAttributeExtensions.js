69999f6020439959efed3b369016115fae8d4334
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-08-22 22:45:03 -0400

    Implemented CustomAttributeExtensions.GetCustomAttribute

--- JSIL.Libraries/Includes/Core/Reflection/Classes/System.Reflection.CustomAttributeExtensions.js
+++ JSIL.Libraries/Includes/Core/Reflection/Classes/System.Reflection.CustomAttributeExtensions.js
@@ -3,6 +3,10 @@
       $.Method({ Static: true, Public: true }, "GetCustomAttribute",
         new JSIL.MethodSignature($jsilcore.TypeRef("System.Attribute"), [$jsilcore.TypeRef("System.Reflection.MemberInfo"), $jsilcore.TypeRef("System.Type"), $.Boolean], []),
         function GetCustomAttribute(element, attributeType, inherit) {
+            // FIXME: Check if TypeInfo is passed in, and if so, cast to type. Hack because TypeInfo in JSIL doesn't use Type as it's base class
+            if (attributeType.FullName === "System.Reflection.TypeInfo")
+                attributeType = attributeType.AsType();
+
             var attributes = element.GetCustomAttributes(attributeType, inherit);
             if (attributes.length === 0) {
                 return null;
